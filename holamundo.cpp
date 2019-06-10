c3e4c4f5100eb0e3bc942d85797888560b3f81e2
Sebastian Bastos <jusebacru@gmail.com>
2019-06-10 11:22:33 -0500

    FUncion para sumar

--- holamundo.cpp
+++ holamundo.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
 using namespace std;
-
+int sumarnum (int a, int b)
+{
+    int resultado=0;
+    resultado = a + b;
+    return resultado;
+}
 
 int main(){
 
+    a = 5 ;
+    b = 7;
+    sumarnum(a,b);
     return 0;
 }
