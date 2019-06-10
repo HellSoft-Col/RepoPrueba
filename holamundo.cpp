d8ce146297d7c6f93cc3b21b09c6018276cb021b
Amanda Soto <amancarolina.sv01@gmail.com>
2019-06-10 11:23:12 -0500

    sumar

--- holamundo.cpp
+++ holamundo.cpp
@@ -3,7 +3,16 @@
 using namespace std;
 
 
-int main(){
+int suma(int x, int y);
 
+int main(){
+    int num1=8;
+    int num2=5;
+
+    suma(num1,num2);
     return 0;
+}
+
+int suma(int x, int y){
+    return x+y;
 }
